6e164b1bd4411bba596358d30a21c34b59357de3
marcguebel <contact@guebel-marc.fr>
2023-07-19 21:26:03 +0200

    gestion model event / race

---
+++ models/Event.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator')
+
+const eventSchema = mongoose.Schema({
+    name : { type: String, required: true },
+    date :  Date,
+    country :  String,
+    location : String,
+    longitude : Number,
+    latitude : Number,
+    country : String
+}, { versionKey: false });
+
+eventSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('Event', eventSchema);
--- models/Race.js
+++ models/Race.js
@@ -2,14 +2,9 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator')
 
 const raceSchema = mongoose.Schema({
-    name : { type: String, required: true },
-    date :  Date,
-    inscriptionOpen : Boolean,
-    description : String,
-    url : String,
-    location : String,
-    longitude : Number,
-    latitude : Number
+    distance : { type: Number, required: true },
+    elevationGain : Number,
+    type : ['trail', 'road']
 }, { versionKey: false });
 
 raceSchema.plugin(uniqueValidator);
